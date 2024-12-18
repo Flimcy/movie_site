c2d459066cd2bb1122698e165e49bc60c3ebfc34
Flimcy <121536004+Flimcy@users.noreply.github.com>
2024-12-18 20:06:41 +1100

    hjjh

--- movie_site/settings.py
+++ movie_site/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r2ul-56j%3n-f%prg7b7$&_fwani^mn-hes12uia&fqycy8dk('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
